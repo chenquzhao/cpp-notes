5f350484e8984b4b5472c5ccc5c48506ba667ebd
Chenqu Zhao <37530543+chenquzhao@users.noreply.github.com>
2020-03-12 10:53:23 +0800

    Update 1_array.cpp

--- ch04 Array/1_array.cpp
+++ ch04 Array/1_array.cpp
@@ -5,6 +5,9 @@ int main()
 	// define array: element type (char/short/int/cloat/double/...), array name, element amounts
 	char arr[5];
 	char arr_[5] = {90,91,92,93,94}; // one char map to 1 Byte in storage
+	char b[5] = {90,91}; // define first 2 elements only
+	char c[5] = {0}; // define all elements as 0
+	char d[] = {1,2,3,4,5}; // sizeof(d) == 5
 
 	// access to array (read)
 	char s1 = arr_[0];
