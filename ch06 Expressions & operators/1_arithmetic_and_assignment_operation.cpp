b085e9ea7e8ec77473f4a9429544c551af2aaf81
Chenqu Zhao <37530543+chenquzhao@users.noreply.github.com>
2020-03-12 18:52:40 +0800

    Create 1_arithmetic_and_assignment_operation.cpp

---
+++ ch06 Expressions & operators/1_arithmetic_and_assignment_operation.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{	
+	// basic math calculation
+	3 + 4; // 7
+	3 - 4; // -1
+	3 * 4; // 12
+	0.2 + 0.4; // 0.6
+	0.2 - 0.4; // -0.2
+	0.2 * 0.4; // 0.08
+
+	// module (integer only) & division
+	22 / 5; // 4
+	22 % 5; // 2
+	22.0 / 5.0; // 4.4
+
+	// assigning operator: '='
+	int a = 10; // left value -> storage, right value -> value
+	int b = 2;
+	a += b; // a = a + b
+	printf("a: %d", a = 100);
+
+	return 0;
+}
