58ad128b5dfe39a37baf956d402fcd5524d57d9b
Chenqu Zhao <37530543+chenquzhao@users.noreply.github.com>
2020-03-12 08:37:39 +0800

    Update 3_multiple_inputs.cpp

--- ch02 Console input & output/3_multiple_inputs.cpp
+++ ch02 Console input & output/3_multiple_inputs.cpp
@@ -18,13 +18,5 @@ int main()
 	scanf_s("%d,%d,%lf", &x, &y, &z); // input must be "x, y, z" in one line
 	printf("x = %d, y = %d, z = %lf \n", x, y, z);
 
-	// Ex. re-formatting date
-	int year;
-	int month;
-	int day;
-	printf("Please input a date: ");
-	scanf_s("%d/%d/%d", &year, &month, &day);
-	printf("The date is: %d-%d-%d", year, month, day);
-
 	return 0;
 }
