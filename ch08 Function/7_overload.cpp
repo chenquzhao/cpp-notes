ed07cab8d0f194ff8463fa3aed8e8a53921ae56f
Chenqu Zhao <37530543+chenquzhao@users.noreply.github.com>
2020-03-19 11:07:07 +0800

    Create 7_overload.cpp

---
+++ ch08 Function/7_overload.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>	
+
+// funciton overload (not allowed in C)
+double find_max(double a, double b)
+{
+	return 0;
+}
+
+double find_max(double a, double b, double c) // parameter amounts (or type) difference
+{
+	return 0;
+}
+
+int main() 
+{	
+	return 0;
+}
