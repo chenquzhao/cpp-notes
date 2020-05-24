f445180e909678c25b1617aab662fd6e9c3d3a19
Chenqu Zhao <37530543+chenquzhao@users.noreply.github.com>
2020-05-24 12:22:47 +0800

    Create 1_define_linked_list.cpp

---
+++ ch13 Linked List/1_define_linked_list.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+struct Student
+{
+	int id;
+	char name[16];
+	Student* next;
+};
+
+Student s[4] =
+{
+	{202001, "John", 0},
+	{202002, "David", 0},
+	{202003, "Chris", 0},
+	{202004, "Patrick", 0}
+};
+
+// find student with specified id
+Student* find(Student* s,int id)
+{
+	while (s)
+	{
+		if (s->id == id)
+			return s;
+
+		s = s->next;
+	}
+
+	return NULL;
+}
+
+int main()
+{
+	// create a LinkedList
+	s[0].next = &s[1];
+	s[1].next = &s[2];
+	s[2].next = &s[3];
+	s[3].next = 0;
+
+	// traverse LinkedList
+	Student* p = &s[0];
+	while (p)
+	{
+		printf("ID: %d, Name: %s\n", p->id, p->name);
+		p = p->next;
+	}
+
+	Student* target = find(&s[0], 202002);
+	printf("Target student is %s", target->name);
+
+	return 0;
+}
